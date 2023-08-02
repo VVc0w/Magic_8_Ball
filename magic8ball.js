0d82158d3098abda5d8177c74f8c66250af871ef
Darren Greenhough <134498109+VVc0w@users.noreply.github.com>
2023-08-02 12:36:38 +0100

    Add files via upload
    This is JavaScript only, in the near future I will be integrating this with HTML and CSS making it into an interactive site. I'll also change the code when I learn a simpler way to do it, at present this was coded using conditionals. The code generates a random number between 0 and 200 and the answers that the 8-Ball gives are determined on which number the code randomly spits out!

---
+++ magic8ball.js
@@ -0,0 +1,86 @@
+var questionVar = prompt("What is your question?")
+console.log(question);
+
+var questionVar = Math.random();
+questionVar = questionVar * 200;
+questionVar = Math.floor(questionVar) + 1;
+
+if (questionVar >= 0 && questionVar <= 9) {
+   alert("Ask again later");
+} 
+
+if (questionVar >= 10 && questionVar <= 19) {
+   alert("Yes");
+} 
+
+if (questionVar >= 20 && questionVar <= 29) {
+   alert("My reply is no");
+} 
+
+if (questionVar >= 30 && questionVar <= 39) {
+   alert("Cannot predict now");
+} 
+
+if (questionVar >= 40 && questionVar <= 49) {
+   alert("You may rely on it");
+} 
+
+if (questionVar >= 50 && questionVar <= 59) {
+   alert("Very doubtful");
+} 
+
+if (questionVar >= 60 && questionVar <= 69) {
+   alert("Not sure");
+} 
+
+if (questionVar >= 70 && questionVar <= 79) {
+   alert("Definitely");
+} 
+
+if (questionVar >= 80 && questionVar <= 89) {
+   alert("You already know the answer");
+} 
+
+if (questionVar >= 90 && questionVar <= 99) {
+   alert("It is decidedly so");
+} 
+
+if (questionVar >= 100 && questionVar <= 109) {
+   alert("Outlook not so good");
+} 
+
+if (questionVar >= 110 && questionVar <= 119) {
+   alert("Without a doubt");
+} 
+
+if (questionVar >= 120 && questionVar <= 129) {
+   alert("Maybe");
+} 
+
+if (questionVar >= 130 && questionVar <= 139) {
+   alert("Only if you focus on it");
+} 
+
+if (questionVar >= 140 && questionVar <= 149) {
+   alert("Signs point to yes");
+} 
+
+if (questionVar >= 150 && questionVar <= 159) {
+   alert("That seems possible");
+} 
+
+if (questionVar >= 160 && questionVar <= 169) {
+   alert("Ask again later");
+} 
+
+if (questionVar >= 170 && questionVar <= 179) {
+   alert("My reply is no");
+} 
+
+if (questionVar >= 180 && questionVar <= 189) {
+   alert("Cannot predict now");
+} 
+
+if (questionVar >= 190 && questionVar <= 200) {
+   alert("Yes");
+} 
